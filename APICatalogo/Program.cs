7f1ebb5d2b523b5041890dea4d790828d5b442ab
JPMedeiros42 <medeirosjpro@hotmail.com>
2025-04-25 12:40:41 -0300

    Migrations, Controller e algumas alterações

--- APICatalogo/Program.cs
+++ APICatalogo/Program.cs
@@ -1,11 +1,31 @@
+using APICatalogo.Context;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(options =>
+    options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+/*------------------------------------------------------------------------------------------*/
+//Banco de Dados SQL Server
+//Configurar a string de conexão com o banco de dados SQL Server
+string connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+//Checar se a string de conexão é nula ou vazia
+if (string.IsNullOrEmpty(connectionString))
+{
+    throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+}
+// Registrar o DBContext com o SQL Server
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(connectionString));
+/*------------------------------------------------------------------------------------------*/
 
 var app = builder.Build();
 
