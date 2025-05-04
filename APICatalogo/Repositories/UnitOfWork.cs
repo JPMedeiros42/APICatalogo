3d64fc3a3a34de9f226755d310a09c555b741ce0
João Pedro Medeiros Santos <peterjp97@hotmail.com>
2025-05-04 14:04:48 -0300

    Unit of Work

---
+++ APICatalogo/Repositories/UnitOfWork.cs
@@ -0,0 +1,45 @@
+﻿using APICatalogo.Context;
+
+namespace APICatalogo.Repositories;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private IProdutoRepository? _produtoRepo;
+    private ICategoriaRepository? _categoriaRepo;
+
+    public AppDbContext _context;
+
+    public UnitOfWork(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    //Lazy Loading
+    public IProdutoRepository ProdutoRepository
+    {
+        get
+        {
+            // Se não existe uma instância de ProdutoRepository, cria uma nova
+            return _produtoRepo ??= new ProdutoRepository(_context);
+        }
+    }
+
+    public ICategoriaRepository CategoriaRepository
+    {
+        get
+        {
+            // Se não existe uma instância de CategoriaRepository, cria uma nova
+            return _categoriaRepo ??= new CategoriaRepository(_context);
+        }
+    }
+
+    public void Commit()
+    {
+        _context.SaveChanges();
+    }
+
+    public void Dispose()
+    {
+        _context.Dispose();
+    }
+}
