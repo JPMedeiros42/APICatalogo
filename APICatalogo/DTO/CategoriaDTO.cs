bc97cc14d279dfcca8f864f165856876ed93f883
João Pedro Medeiros Santos <peterjp97@hotmail.com>
2025-05-07 16:19:19 -0300

    DTOs

---
+++ APICatalogo/DTO/CategoriaDTO.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APICatalogo.DTO;
+
+public class CategoriaDTO
+{
+    public int CategoriaID { get; set; }
+
+    [Required]
+    [StringLength(60)]
+    public string? Nome { get; set; }
+
+    [Required]
+    [StringLength(300)]
+    public string? ImagemUrl { get; set; }
+}
